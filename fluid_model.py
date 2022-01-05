6b5bf0c5524cef27c314a969dd5606c2c7b57a79
Xiangyu Zhang <zhangxiangyu1995@outlook.com>
2022-01-04 21:59:42 -0500

    add isfeasible function

--- fluid_model.py
+++ fluid_model.py
@@ -102,3 +102,66 @@ class fluid_model():
             states = pull @ P1 + idle @ P0
             assert(states.shape == (num_states, ))
         return rewards
+    
+    def is_feasible(self, active, tie_idx):
+        params = self.params
+        num_actions, num_states, T, gamma, r, init_occupation, P0, P1, budgets =\
+            params["num_actions"], params["num_states"], params["T"], params["gamma"], \
+            params["r"], params["init_occupation"], params["P0"], params["P1"], \
+            params["budgets"]
+
+        assert(isinstance(active, list))
+        assert(isinstance(tie_idx, int))
+        assert(0 <= tie_idx <= num_states - 1)
+
+        # V = [0] * num_states
+        # lambd = 0
+
+        # V(s) = max_a {r(s, a) - lambd * a + gamma * V[s' | s, a]}
+        # left = V = left_matrix @ V
+        left_matrix = np.identity(num_states)
+
+        # right_0 = r[:, 0] + gamma * P0 @ V
+        # right_1 = r[:, 1] - lambd + gamma * P1 @ Vrig
+        right_0_adding = r[:, 0]
+        right_0_lambda = np.array([0] * num_states)
+        right_0_matrix = gamma * P0
+
+        right_1_adding = r[:, 1]
+        right_1_lambda = np.array([-1] * num_states)
+        right_1_matrix = gamma * P1
+
+
+        right_adding = [right_1_adding[idx] if active[idx] else right_0_adding[idx] for idx in range(num_states)]
+        right_lambda = [right_1_lambda[idx] if active[idx] else right_0_lambda[idx] for idx in range(num_states)]
+        right_matrix = [right_1_matrix[idx] if active[idx] else right_0_matrix[idx] for idx in range(num_states)]
+
+        A = np.zeros((num_states + 1, num_states + 1))
+        b = np.zeros((num_states + 1,))
+        A[0: num_states, 0: num_states] = left_matrix - right_matrix
+        A[0: num_states, -1] = -np.array(right_lambda)
+        b[0: num_states] = right_adding
+
+        A[-1, 0: num_states] = right_0_matrix[tie_idx] - right_1_matrix[tie_idx]
+        A[-1, -1] = right_0_lambda[tie_idx] - right_1_lambda[tie_idx]
+        b[-1] = right_1_adding[tie_idx] - right_0_adding[tie_idx]
+
+        sol = np.linalg.solve(A, b)
+
+        v, lambd = sol[0: num_states], sol[-1]
+
+        active_reward = right_1_adding + right_1_lambda * lambd + right_1_matrix @ v
+
+        inactive_reward = right_0_adding + right_0_lambda * lambd + right_0_matrix @ v
+
+        def is_greater_than(a, b):
+            return a >= b or np.isclose(a, b)
+
+        for idx, status in enumerate(active):
+            if status == True:
+                assert(is_greater_than(active_reward[idx], inactive_reward[idx]))
+            else:
+                assert(is_greater_than(inactive_reward[idx], active_reward[idx]))
+        
+        return True, lambd
+        
